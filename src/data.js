fc9723148e80ccfc0dd720864616ab7cc7e36405
jackelizeth <jackeline-chavez-m@hotmail.com>
2018-06-20 00:47:55 -0500

    mostrando el cohort user y progress

--- src/data.js
+++ src/data.js
@@ -1,1 +1,29 @@
-window.b()
+window.data = {
+
+    computeUsersStats(users, progress, courses) = {
+        for(var i = 0; i < 3; i++){
+
+        }
+        
+     
+    }
+
+    sortUsers(users, orderBy, orderDirection) = {
+
+    }
+
+
+    filterUsers(users, search) = {
+
+    }
+
+    progressCohortsData(users, progress, courses) = {
+
+    }
+
+
+};
+
+
+
+    
--- src/main.js
+++ src/main.js
@@ -1,30 +1,73 @@
-const form = document.getElementById('mi_form');
-const miField = document.getElementById('mi_keyword');
-const miResponContainer = document.getElementById('respon_container');
-let miForText;
+const form_1 = document.getElementById('form-1');
+const field_1 = document.getElementById('keyword-1');
 
+const form_2 = document.getElementById('form-2');
+const field_2 = document.getElementById('keyword-2');
+ 
+const form_3 = document.getElementById('form-3');
+const field_3 = document.getElementById('keyword-3');
+ 
+const responContainer = document.getElementById('respon-container');
 
-form.addEventListener('submit', function (e) {
+let option;
+
+form_1.addEventListener('submit', function (e) {
     e.preventDefault(),
-    miResponContainer.innerHTML='';
-    miForText = miField.value;
-    getNews();
+    responContainer.innerHTML='';
+    option ="cohort";
+    getData('/data/cohorts.json');
 });
 
-function getNews () { 
- //   alert(1233344)
-var xhttp = new XMLHttpRequest();
-    xhttp.open('GET','http://127.0.0.1:8080/data/cohorts.json')
-    xhttp.onload = addNews;
-    xhttp.onerror = handleError;
-    xhttp.send();
+form_2.addEventListener('submit', function (e) {
+    e.preventDefault(),
+    responContainer.innerHTML='';
+    option ="user";
+    getData('/data/cohorts/lim-2018-03-pre-core-pw/users.json');
+});
+
+form_3.addEventListener('submit', function (e) {
+    e.preventDefault(),
+    responContainer.innerHTML='';
+    option ="progress";
+    getData('/data/cohorts/lim-2018-03-pre-core-pw/progress.json');
+});
+
+function getData (Url) { 
+
+var xhr = new XMLHttpRequest();
+    xhr.open("GET", Url , true)
+    xhr.onload = dataNew;
+    xhr.onerror = dataError;
+    xhr.send();
+}
+
+function dataNew () {    
+    let listado = "";
+    if(option =="cohort"){
+        //responText retorna la respuesta como string
+        //JSON.parse lo convierten a un objeto JSON
+        const data = JSON.parse(this.responseText);
+        //console.log(data)
+        data.forEach(block => {
+            listado +=  block.id+"<br>";
+        });
+        
+    }else if(option == "user"){
+        const data = JSON.parse(this.responseText);
+        //console.log(data)
+        data.forEach(block => {
+            listado += block.name +"---"+ block.role+"<br>";
+        });
+
+    }else{
+        const data = Object.keys(JSON.parse(this.responseText)) ;
+        //console.log(data)
+        data.forEach(block => {
+            listado += block +"<br>";
+        });
     }
- 
-function addNews () {
-    const data = JSON.parse(this.responseText);
-    miResponContainer.innerHTML= data;
+    responContainer.innerHTML= listado ;
 } 
-   
-function handleError(){
+function dataError(){
         console.log ('Se ha producido un error');
-    }
+}
