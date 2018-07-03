4e0cd3b242f3a6a0eeb7bc2e903b407dbf15e2b7
NancyMontalvo21 <39255748+NancyMontalvo21@users.noreply.github.com>
2018-07-03 00:06:17 -0500

    Modificando codigo de botones

--- src/main.js
+++ src/main.js
@@ -1,28 +1,7 @@
 
 // Boton despegable
 
-// $(document).ready(function(){
-//     $('.menu li:has(ul)').click(function(e){
-//         e.preventDefault();
-    
-//         if ($(this).hasClass('activado')) {
-//             $(this).removeClass('activado');
-//             $(this).children('ul').slideUp();
- 
-//         } else {
- 
-//             $('.menu li ul').slideUp();
-//             $('.menu li').removeClass('activado');
-//             $(this).addClass('activado');
-//             $(this).children('ul').slideDown();
- 
-//          }
-//         });
 
-
-// const inputText = document.getElementById('inp-text-1');
-// const inputBtn = document.getElementById('inp-btn-1');
-// const showInformation = document.getElementById('show-information');
 
 let urlCoh = '../data/cohorts.json';
 
